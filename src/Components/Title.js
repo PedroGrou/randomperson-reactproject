fc94f7ad5ae544aab0b5dfd72d356150b960b836
PedroGrou <pedrogroug@gmail.com>
2021-07-24 18:43:36 +0100

    initial

---
+++ src/Components/Title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Title() {
+  return (
+    <div className="section-title">
+      <h1>Review</h1>
+      <div className="underline"></div>
+    </div>
+  );
+}
+
+export default Title;
